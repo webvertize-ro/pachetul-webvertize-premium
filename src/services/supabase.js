71cd89e5bd5dbfb30298cebf5fd80b8c0ca59cc6
webvertize-ro <contact@webvertize.ro>
2026-03-23 12:38:13 +0200

    further changes

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js/dist/index.cjs';
 
-export const supabaseUrl = 'https://ebsaptaehndiwvjdbqnm.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVic2FwdGFlaG5kaXd2amRicW5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3Nzg2NzYsImV4cCI6MjA4ODM1NDY3Nn0.0jrXRXjWnCB-Q1U8asSFYHNs_ojhmkQJfJeWX35mYJI';
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
