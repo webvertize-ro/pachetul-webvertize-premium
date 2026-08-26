c500dbd78e6ccbefdc19f31a3e9bf4d3de7a505a
webvertize-ro <contact@webvertize.ro>
2026-08-27 00:19:19 +0300

    logo width adjustment

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,30 +1,35 @@
 import styled from "styled-components";
-import logoImg from "../assets/images/basic-business-logo.svg";
-import logoImgDark from "../assets/images/basic-business-logo-dark.svg";
+import { c, cWidth } from "../utils/content";
 import { useContent } from "../hooks/useContent";
-import { c } from "../utils/content";
 
-const ImgContainer = styled.div`
-  display: flex;
-  justify-content: center;
-`;
+const DEFAULT_WIDTH = 160;
+const MIN_WIDTH = 40;
+const MAX_WIDTH = 400;
 
 const StyledImg = styled.img`
-  max-width: ${(props) => (props.width ? props.width : "160px")};
-  max-height: ${(props) => (props.width ? props.width : "60px")};
+  width: ${(props) => props.$width}px;
+  height: auto;
+  display: block;
 `;
 
-function Logo({ width, dark }) {
+function Logo() {
   const { contentMap } = useContent();
 
+  const rawWidth = cWidth(contentMap, "global.logo");
+  const width =
+    Number.isFinite(rawWidth) && rawWidth >= MIN_WIDTH && rawWidth <= MAX_WIDTH
+      ? rawWidth
+      : DEFAULT_WIDTH;
+
+  const src = c(contentMap, "global.logo");
+  console.log("src in Logo: ", src);
+
   return (
-    <ImgContainer>
-      <StyledImg
-        width={width}
-        src={c(contentMap, "global.logo")}
-        alt="Logo-ul afacerii [Nume Afacere]"
-      />
-    </ImgContainer>
+    <StyledImg
+      $width={width}
+      src={c(contentMap, "global.logo")}
+      alt="Logo-ul afacerii [Nume Afacere]"
+    />
   );
 }
 
